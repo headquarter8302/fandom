4e8ec7dd9385fb5d5e4e7baf7f559c44206355e6
Markus <65797058+headquarter8302@users.noreply.github.com>
2021-05-20 13:08:53 +0800

    Create global.js

---
+++ personal/global.js
@@ -0,0 +1,17 @@
+importArticles({
+    type: 'script',
+    articles: [
+        'u:dev:MediaWiki:PurgeButton/code.js',
+        'u:dev:MediaWiki:View_Source/code.js',
+        'u:dev:MediaWiki:EditProfilesButton/code.js',
+        'u:dev:MediaWiki:Sofix.js',
+        'u:dev:MediaWiki:View_Raw/code.js',
+        'u:dev:MediaWiki:UserAccountAge/code2.js',
+        'u:dev:MediaWiki:SoundcloudPlayer.js',
+        'u:dev:MediaWiki:ToggleSideBar/code.js',
+    ]
+});
+
+window.Sofix = {
+    enablePersonalUse: true
+};
